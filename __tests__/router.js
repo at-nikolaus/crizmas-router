d3151d2d218335310ea2ce137a3c6fadbe162c3d
Raul-Sebastian Mihaila <raul.mihaila@gmail.com>
2017-09-13 09:03:04 +0300

    wip

--- __tests__/router.js
+++ __tests__/router.js
@@ -1,27 +1,34 @@
 const Router = require('../src/router.js');
 const url = require('url');
 
-beforeAll(() => {
-  global.URLSearchParams = url.URLSearchParams;
-});
+global.URLSearchParams = url.URLSearchParams;
 
 describe('router', () => {
   describe('Router', () => {
     test('works', () => {
+      const mockFn = jest.fn();
+
       const router = new Router({
         routes: [
           {
             path: '*',
             component: () => false
+          },
+          {
+            path: 'home',
+            component: () => false,
+            controller: {
+              onEnter() {
+                mockFn();
+              }
+            }
           }
         ]
       });
 
-      router.onChange(() => {
-        console.log('route changed', router.currentRouteFragment);
-      });
-
       router.mount();
+      router.transitionTo('home');
+      expect(mockFn.mock.calls.length).toBe(1);
     });
   });
 });
